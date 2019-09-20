0c187782fd3ac741576a72b7d4d82faf5b4c73f6
Damini Varu <damvaru4@gmail.com>
2019-09-20 17:39:57 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,8 @@
+require 'pry'
+
 class Author 
   
-  @@all = [ ]
+ 
   
   attr_accessor :name 
   
@@ -8,26 +10,24 @@ class Author
   def initialize(name)
     @name = name
     
-    @@all << self 
   end 
   
-  def self.all 
-    @all 
-  end 
   
   def add_post(post)
     post.author = self
   end
+     
+    def post 
+      Post.all.select so |post|
+      post.author == self
+    end  
      
   def add_post_by_title(name)
     post = Post.new(name)
     post.author = self
   end 
     
-  def post 
-      Post.all.select so |i|
-      i.author == self 
-  end 
+  
     
   def self.post_count
     Song.all.length
